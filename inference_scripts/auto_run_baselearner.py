033c43e8bc623ce8c95d37dc2b6eae74f89d543f
hongyusu <hongyu.su.uh@gmail.com>
2014-01-18 23:53:04 +0200

    minor

--- inference_scripts/auto_run_baselearner.py
+++ inference_scripts/auto_run_baselearner.py
@@ -9,6 +9,7 @@ import time
 import logging
 logging.basicConfig(format='%(asctime)s %(filename)s %(funcName)s %(levelname)s:%(message)s', level=logging.INFO)
 
+current_DIR = os.getcwd() 
 
 
 def singleBaselearner(filename,graph_type,t,node):
@@ -17,7 +18,7 @@ def singleBaselearner(filename,graph_type,t,node):
     logging.info('\t--< (node)%s,(f)%s,(type)%s,(t)%s' %( node,filename,graph_type,t))
   except:
     logging.info('\t--> (node)%s,(f)%s,(type)%s,(t)%s' %( node,filename,graph_type,t))
-    os.system(""" ssh -o StrictHostKeyChecking=no %s 'cd /home/group/urenzyme/workspace/sop/mlj_ensemblemmcrf/tree_inference_scripts/; nohup matlab -nodisplay -nosplash -nodesktop -r "run_baselearner '%s' '%s' '%s' '0' " > /var/tmp/tmp_%s_%s_%s_baselearner' """ % (node,filename,graph_type,t,filename,graph_type,t) )
+    os.system(""" ssh -o StrictHostKeyChecking=no %s 'cd %s; nohup matlab -nodisplay -nosplash -nodesktop -r "run_baselearner '%s' '%s' '%s' '0' " > /var/tmp/tmp_%s_%s_%s_baselearner' """ % (node,current_DIR,filename,graph_type,t,filename,graph_type,t) )
     logging.info('\t--| (node)%s,(f)%s,(type)%s,(t)%s' %( node,filename,graph_type,t))
     time.sleep(5)
   pass
